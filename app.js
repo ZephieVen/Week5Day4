56d2cb2d11fcbb16fe590f7b90c0a6e4794a87b8
Anja Schmidt <zephieven@gmail.com>
2021-08-27 10:51:06 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// alert("This is working")
+
+$(".navItem").css("color", "white")
+$("#nav").css("background-color", "#145");
+
+//Number 7
+$("li:even").css("background-color", "#ccc");
+$("li:odd").css("background-color", "#aaa");
+
+$("input").val("Anja")
